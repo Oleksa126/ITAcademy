b8b6383bad1129c427696ff4830df661717b6854
Олександр Барадач <oleksandr892@outlook.com>
2018-02-10 16:48:44 +0200

    add something

--- ItAcademy/Controllers/HomeController.cs
+++ ItAcademy/Controllers/HomeController.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Models;
 
 namespace ItAcademy.Controllers
 {
+    
+
     public class HomeController : Controller
     {
+
+        QuestionContext context = new QuestionContext();
+
         public ActionResult Index()
         {
+            var que = context.questions;
+
+            ViewBag.questions = que;
             return View();
         }
 
---
+++ Models/QuestionContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Data.Entity;
+
+namespace Models
+{
+    public class QuestionContext : DbContext
+    {
+        public DbSet<question> questions { get; set; }
+        
+    }
+}
